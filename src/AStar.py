3647893f0ea9e15001c39096f2fd2bc607d09c1f
AntonRosenberg <Rosenberg8595@gmail.com>
2024-10-06 18:28:55 +0200

    Updated A-star documentation

--- src/AStar.py
+++ src/AStar.py
@@ -15,7 +15,8 @@ def reconstruct_path(came_from, current):
 def a_star_dist(graph, start, goal):
     """Preform A* search on graph using distance as cost"""
     def heuristic(p1, p2):
-        return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5  # Euclidean distance
+        """Heuristic function using euclidian distance to goal"""
+        return ((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2) ** 0.5
 
     open_set = []
     heapq.heappush(open_set, (0, tuple(start)))
@@ -46,7 +47,7 @@ def a_star_time(graph, start, goal, kinematics_model):
     """Perform A* search on the graph with time as costs, including penalty for turns."""
     
     def heuristic(node, goal, acceleration, velocity):
-        """Heuristic function: Time-based estimate (using distance and velocity)."""
+        """Heuristic function: Time-based estimate, derived from kinematic equation for distance."""
         distance = np.sqrt((node[0] - goal[0])**2 + (node[1] - goal[1])**2)
         return (np.sqrt(velocity**2 + 2 * acceleration * distance) - velocity) / acceleration
 
@@ -71,11 +72,9 @@ def a_star_time(graph, start, goal, kinematics_model):
         # Get the node with the lowest f_score
         current_f, current, prev_node, current_velocity = heapq.heappop(open_set)
         
-        # If we've reached the goal, reconstruct the path
         if current == goal:
             return reconstruct_path(came_from, goal)
         
-        # Explore each neighbor of the current node
         for neighbor in graph[current]:
             # Update the kinematics model velocity based on the current velocity
             kinematics_model.velocity = current_velocity
@@ -84,19 +83,13 @@ def a_star_time(graph, start, goal, kinematics_model):
             time_to_neighbor = kinematics_model.time_to_traverse(current, neighbor, prev_node)
             final_velocity = kinematics_model.velocity  # Final velocity at the neighbor after traversal
             
-            # Calculate tentative g_score for this neighbor
             tentative_g_score = g_score[current] + time_to_neighbor
             
-            # If this path to neighbor is better (lower g_score), update the path
             if tentative_g_score < g_score[neighbor]:
                 came_from[neighbor] = current
                 g_score[neighbor] = tentative_g_score
                 v_score[neighbor] = final_velocity  # Update the velocity at the neighbor
-                
-                # Calculate the f_score (g_score + heuristic)
                 f_score[neighbor] = tentative_g_score + heuristic(neighbor, goal, kinematics_model.acceleration, final_velocity)
-                
-                # Push the neighbor to the open set with its f_score, neighbor, current node, and final velocity
                 heapq.heappush(open_set, (f_score[neighbor], neighbor, current, final_velocity))
     
     # No path found
